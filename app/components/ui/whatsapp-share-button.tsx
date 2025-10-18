5347624ce8f1d4ecdb6513bd73c46d8165d23760
Fiqih Alfito <fiqihalfito.dev@gmail.com>
2025-10-18 17:40:27 +0700

    f

--- app/components/ui/whatsapp-share-button.tsx
+++ app/components/ui/whatsapp-share-button.tsx
@@ -11,11 +11,11 @@ export function KirimWAButton({ nomor, idCustomer }: KirimWAButtonProp) {
     const domain = "https://pengecekan-pengunjung-guardian.vercel.app"
     const link = `${domain}/customer/${idCustomer}`
 
-    const pesan = `Halo! 👋
+    const pesan = `Halo!
 Untuk melihat hasil pemeriksaan kesehatan Anda, silakan klik tautan berikut:
 ${link}
 
-Terima kasih dan semoga Anda sehat selalu! 💚`
+Terima kasih dan semoga Anda sehat selalu!`
 
     return (
         <Button size={"sm"} className="bg-green-600 shadow hover:bg-green-700" asChild>
